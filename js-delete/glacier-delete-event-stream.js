d1a69c01588a36a0871f799a46fbddd484beac84
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-10-14 03:55:37 +0000

    create transform

--- js-delete/glacier-delete-event-stream.js
+++ js-delete/glacier-delete-event-stream.js
@@ -1,6 +1,19 @@
 var AWS = require("aws-sdk");
-var es = require('event-stream');
+var split2 = require("split2");
+const util = require('util');
 
+
+const Transform = require('stream').Transform
+class GlacierDelete2 extends Transform {
+    _transform(chunk, encoding, cb) {
+        glacierDelete2(chunk, cb)
+    }
+}
+class GlacierDelete extends Transform {
+    _transform(chunk, encoding, cb) {
+        glacierDelete(chunk, cb)
+    }
+}
 var myConfig = new AWS.Config({
       region: 'us-east-1'
 });
@@ -20,11 +33,11 @@ function glacierDelete2(line, cb){
     let param2 = {accountId:params.accountId, archiveId: line.toString(), vaultName: params.vaultName}
     setTimeout(() => {
       cb(null, "sent " + param2.archiveId + "\n")
-    }, 50)
+    }, 200)
 }
 
 
 process.stdin
-    .pipe(es.split())
-    .pipe(es.map(glacierDelete2))
+    .pipe(split2())
+    .pipe(new GlacierDelete2)
     .pipe(process.stdout)
